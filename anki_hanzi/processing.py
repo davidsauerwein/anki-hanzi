3b276b95ff0da999f3b57d13ec019464b00353cc
David Sauerwein <david@sauerwein.xyz>
2024-10-20 13:16:07 +0200

    Add Pinyin and Zhuyin processig using dragonmapper
    Signed-off-by: David Sauerwein <david@sauerwein.xyz>

--- anki_hanzi/processing.py
+++ anki_hanzi/processing.py
@@ -1,12 +1,68 @@
+from typing import Callable
+
 from anki.notes import Note
+from dragonmapper import hanzi  # type: ignore
 
 ANKI_HANZI_TAG = "anki-hanzi"
 
 
-def process_chinese_vocabulary_note(note: Note, force: bool = False) -> bool:
+def to_pinyin(text: str) -> str:
+    return hanzi.to_pinyin(text, accented=True)  # type: ignore
+
+
+def to_zhuyin(text: str) -> str:
+    return hanzi.to_zhuyin(text)  # type: ignore
+
+
+def transform_field(
+    note: Note,
+    source_field: str,
+    target_field: str,
+    transformation_function: Callable[[str], str],
+    overwrite_target_field: bool = False,
+) -> bool:
+    assert source_field in note and target_field in note
+
+    if not note[source_field]:
+        # Ignore the transformation if source is empty
+        return False
+
+    if note[target_field] and not overwrite_target_field:
+        # Do not overwrite target fields unless explicitly asked.
+        return False
+
+    note[target_field] = transformation_function(note[source_field])
+    return False
+
+
+def process_chinese_vocabulary_note(
+    note: Note, force: bool = False, overwrite_target_field: bool = False
+) -> bool:
     print(f"Processing {note}")
     if not force and note.has_tag(ANKI_HANZI_TAG):
         return False
-    # TODO Call all the different functions
-    # TODO tag notes
+
+    transform_field(
+        note=note,
+        source_field="Word (Traditional Character)",
+        target_field="Word (Pinyin)",
+        transformation_function=to_pinyin,
+        overwrite_target_field=overwrite_target_field,
+    )
+    transform_field(
+        note=note,
+        source_field="Word (Traditional Character)",
+        target_field="Word (Zhuyin)",
+        transformation_function=to_zhuyin,
+        overwrite_target_field=overwrite_target_field,
+    )
+    transform_field(
+        note=note,
+        source_field="Example Sentence - Traditional Characters",
+        target_field="Example Sentence - Zhuyin",
+        transformation_function=to_zhuyin,
+        overwrite_target_field=overwrite_target_field,
+    )
+
+    note.add_tag(ANKI_HANZI_TAG)
     return True
